56a07d53891613c2f0fbd3e1b26c49276b00f651
Oceng212 <dickyalamsyah11120@gmail.com>
2021-07-23 20:40:11 +0700

    perubahan

---
+++ admin/profil-sekolah.php
@@ -0,0 +1,6 @@
+<?php 
+include_once '../templates/header.php';
+?>
+
+
+<?php include_once '../templates/footer.php';?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,11 @@
+<div class="container-fluid mt-5">
+    <div class="row">
+      <div class="col">
+        <center><img src="../img/contact.jpg" class="img-fluid" alt="Responsive image" style="height: 150px;"></center>
+    </div>
+  </div>
+  </div>
+
 </main>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
